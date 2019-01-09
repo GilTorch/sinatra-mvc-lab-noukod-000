97df5095f3616ab1569a35833c5033a789a03dd0
Gilbert Torchon <gilbert@noukod.com>
2019-01-09 16:25:27 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,44 +5,34 @@ class PigLatinizer
    wordsArray=sentence.split(" ")
 
    wordsArray.collect do |word|
-
-    upcased={}
-   word.split("").each_with_index do |w,i|
-     upcased[w.downcase+"-"+i.to_s]=(w==w.upcase)?true:false
-   end
-   word=word.downcase
-   wordArray=word.split("")
-   firstLetter=wordArray.shift
-    if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-      if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-          secondLetter=wordArray.shift
-
-         if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-          thirdLetter=wordArray.shift
-          wordArray.push(firstLetter,secondLetter,thirdLetter)
-          wordArray.push("a","y")
-          else
-          wordArray.push(firstLetter,secondLetter)
-          wordArray.push("a","y")
-         end
-      else
-       wordArray.push(firstLetter)
-       wordArray.push("a","y")
-      end
-    else
-     if(firstLetter.scan(/[aeiouy]/)!=[])
-       wordArray.unshift(firstLetter)
-       wordArray.push("w","a","y")
+     wordArray=word.split("")
+     firstLetter=wordArray.shift
+     if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/i)!=[])
+       if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+           secondLetter=wordArray.shift
+             
+           if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+           thirdLetter=wordArray.shift
+           wordArray.push(firstLetter,secondLetter,thirdLetter)
+           wordArray.push("a","y")
+           else 
+           wordArray.push(firstLetter,secondLetter)
+           wordArray.push("a","y")
+           end
+       else
+         wordArray.push(firstLetter)
+         wordArray.push("a","y")
+       end
+     else
+       if(firstLetter.scan(/[aeiouy]/i)!=[])
+         wordArray.unshift(firstLetter)
+         wordArray.push("w","a","y")
+       end
      end
-    end
 
-    wordArray=wordArray.each_with_index.collect do |w,i|
-     w=upcased[w+"-"+i.to_s]?w.upcase : w.downcase
-     w
-    end
-
-    wordArray.join("")
+     wordArray.join("")
     end.join(" ")
  end
+
 
   end
