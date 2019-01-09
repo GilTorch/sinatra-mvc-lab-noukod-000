2a17679d9e04b8a64b9f4621dc4a06eed4502175
Gilbert Torchon <gilbert@noukod.com>
2019-01-09 15:04:47 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,27 +2,36 @@ class PigLatinizer
 
 
   def piglatinize(word)
-     word=word.downcase
-     wordArray=word.split("")
-     firstLetter=wordArray.shift
-      if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-        if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-             secondLetter=wordArray.shift
-            wordArray.push(firstLetter,secondLetter)
-            wordArray.push("a","y")
-        else
-         wordArray.push(firstLetter)
-         wordArray.push("a","y")
-        end
-      else
-       if(firstLetter.scan(/[aeiouy]/)!=[])
-         wordArray.push(firstLetter)
-         wordArray.push("w","a","y")
+
+    upcased={}
+    word.split("").each do |w|
+      upcased[w.downcase]=(w==w.upcase)?true:false
+    end
+
+    word=word.downcase
+    wordArray=word.split("")
+    firstLetter=wordArray.shift
+     if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+       if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+            secondLetter=wordArray.shift
+           wordArray.push(firstLetter,secondLetter)
+           wordArray.push("a","y")
+       else
+        wordArray.push(firstLetter)
+        wordArray.push("a","y")
        end
+     else
+      if(firstLetter.scan(/[aeiouy]/)!=[])
+        wordArray.unshift(firstLetter)
+        wordArray.push("w","a","y")
       end
+     end
 
-      wordArray.join("")
+     wordArray=wordArray.collect do |w|
+      w=upcased[w]?w.upcase : w.downcase
+      w
+     end
 
-   end
+     wordArray.join("")
 
-end
+  end
