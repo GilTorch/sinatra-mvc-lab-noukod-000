166c205f08b7e6ac7479fb6daac362a92c2f4c78
Gilbert Torchon <gilbert@noukod.com>
2019-01-09 16:25:33 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -10,12 +10,12 @@ class PigLatinizer
      if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/i)!=[])
        if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
            secondLetter=wordArray.shift
-             
+
            if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
            thirdLetter=wordArray.shift
            wordArray.push(firstLetter,secondLetter,thirdLetter)
            wordArray.push("a","y")
-           else 
+           else
            wordArray.push(firstLetter,secondLetter)
            wordArray.push("a","y")
            end
