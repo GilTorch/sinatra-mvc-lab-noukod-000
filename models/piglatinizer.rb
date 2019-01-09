ab7e20a635ada0db58a9cb0c9737526602550ad6
Gilbert Torchon <gilbert@noukod.com>
2019-01-09 15:25:32 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,46 +1,50 @@
 class PigLatinizer
 
 
-  def piglatinize(word)
+  
+  def piglatinize(sentence)
+    wordsArray=sentence.split(" ")
 
-   upcased={}
-   word.split("").each_with_index do |w,i|
-     upcased[w.downcase+"-"+i.to_s]=(w==w.upcase)?true:false
-   end
-   puts upcased
-   word=word.downcase
-   wordArray=word.split("")
-   firstLetter=wordArray.shift
-    if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-      if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-          secondLetter=wordArray.shift
+    wordsArray.collect do |word|
 
-         if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
-          thirdLetter=wordArray.shift
-          wordArray.push(firstLetter,secondLetter,thirdLetter)
-          wordArray.push("a","y")
-          else
-          wordArray.push(firstLetter,secondLetter)
-          wordArray.push("a","y")
-         end
-      else
-       wordArray.push(firstLetter)
-       wordArray.push("a","y")
+     upcased={}
+    word.split("").each_with_index do |w,i|
+      upcased[w.downcase+"-"+i.to_s]=(w==w.upcase)?true:false
+    end
+    puts upcased
+    word=word.downcase
+    wordArray=word.split("")
+    firstLetter=wordArray.shift
+     if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+       if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+           secondLetter=wordArray.shift
+             
+          if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+           thirdLetter=wordArray.shift
+           wordArray.push(firstLetter,secondLetter,thirdLetter)
+           wordArray.push("a","y")
+           else 
+           wordArray.push(firstLetter,secondLetter)
+           wordArray.push("a","y")
+          end
+       else
+        wordArray.push(firstLetter)
+        wordArray.push("a","y")
+       end
+     else
+      if(firstLetter.scan(/[aeiouy]/)!=[])
+        wordArray.unshift(firstLetter)
+        wordArray.push("w","a","y")
       end
-    else
-     if(firstLetter.scan(/[aeiouy]/)!=[])
-       wordArray.unshift(firstLetter)
-       wordArray.push("w","a","y")
      end
-    end
 
-    wordArray=wordArray.each_with_index.collect do |w,i|
-     w=upcased[w+"-"+i.to_s]?w.upcase : w.downcase
-     w
-    end
+     wordArray=wordArray.each_with_index.collect do |w,i|
+      w=upcased[w+"-"+i.to_s]?w.upcase : w.downcase
+      w
+     end
 
-    wordArray.join("")
-
- end
+     wordArray.join("")
+     end.join(" ")
+  end
 
   end
