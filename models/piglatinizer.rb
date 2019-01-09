aa2299e1b75000e093a6933c3513ffc44c5b047f
Gilbert Torchon <gilbert@noukod.com>
2019-01-09 14:43:07 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,34 +2,26 @@ class PigLatinizer
 
 
   def piglatinize(word)
-    wordArray=word.split("")
-    # check the first letter of the word to see if it's a consonant or vowel
-    firstLetter=wordArray.shift
-     # if its a consonant check if the second letter is a consonant too
-     if(firstLetter.scan(/bcdfghjklmnpqrstvwxz/)!=[])
-       if(wordArray[0].scan(/bcdfghjklmnpqrstvwxz/)!=[])
-         # if the second letter is a consonant and remove the two first consonants from the start of the word
-            secondLetter=wordArray.shift
-           # put them to the end
-           wordArray.push(firstLetter,secondLetter)
-           # add "ay" to them
-           wordArray.push("a","y")
-       else
-        # if the second letter is not a consonant then do the same steps for the consonant:
-        # put them to the end
-        wordArray.push(firstLetter)
-        # add "ay" to them
-        wordArray.push("a","y")
+     wordArray=word.split("")
+     firstLetter=wordArray.shift
+      if(firstLetter.scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+         puts "I am a consonant"
+        if(wordArray[0].scan(/[bcdfghjklmnpqrstvwxz]/)!=[])
+             secondLetter=wordArray.shift
+            wordArray.push(firstLetter,secondLetter)
+            wordArray.push("a","y")
+        else
+         wordArray.push(firstLetter)
+         wordArray.push("a","y")
+        end
+      else
+       if(word[0].scan(/[aeiouy]/)!=[])
+         wordArray.push("w","a","y")
        end
-     else
-      # if the first letter is a vowel just add "way" to the end of the string
-      if(word[0].scan(/[aeiouy]/)!=[])
-        wordArray.push("w","a","y")
       end
-     end
-
-     wordArray.join("")
-
-  end
+ 
+      wordArray.join("")
+ 
+   end
 
 end
