2944e6cae604888c31675314ce9ac1593f045c6b
Gilbert Torchon <gilbert@noukod.com>
2019-01-09 14:34:50 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -2,9 +2,32 @@ class PigLatinizer
 
 
   def piglatinize(word)
-    words=word.split("")
-    first_letter=words.shift
-    words+first_letter+"ay"
+    wordArray=word.split("")
+    # check the first letter of the word to see if it's a consonant or vowel
+    firstLetter=wordArray.shift
+     # if its a consonant check if the second letter is a consonant too 
+     if(firstLetter.scan(/bcdfghjklmnpqrstvwxz/)!=[])
+       if(wordArray[0].scan(/bcdfghjklmnpqrstvwxz/)!=[])
+         # if the second letter is a consonant and remove the two first consonants from the start of the word 
+            secondLetter=wordArray.shift
+           # put them to the end 
+           wordArray.push(firstLetter,secondLetter)
+           # add "ay" to them
+           wordArray.push("a","y")
+       else 
+        # if the second letter is not a consonant then do the same steps for the consonant
+        # put them to the end 
+        wordArray.push(firstLetter)
+        # add "ay" to them
+        wordArray.push("a","y")
+       end
+     else
+       
+      # if the first letter is a vowel just add "way" to the end of the string
+     end
+    
+    
+        
   end
 
 end
